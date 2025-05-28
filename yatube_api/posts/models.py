2d4e5adf6196dab636746070ab30a0030cc647ba
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-05-28 19:08:23 +0600

    Добавить связь между группами и постами

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.TextField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -11,6 +21,10 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL,
+        related_name='posts', blank=True, null=True
+    )
 
     def __str__(self):
         return self.text
@@ -24,12 +38,3 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
-
-
-class Group(models.Model):
-    title = models.TextField(max_length=200)
-    slug = models.SlugField(unique=True, max_length=50)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
