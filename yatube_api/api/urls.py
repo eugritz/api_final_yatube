26855c52c623b003293288e19765a7d56cfe56da
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-05-28 18:41:06 +0600

    Добавить API для получения групп

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,13 @@
 from django.urls import include, path
+from rest_framework import routers
+
+from .viewsets import GroupViewSet
+
+
+router = routers.DefaultRouter()
+router.register('groups', GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include('djoser.urls.jwt')),
 ]
